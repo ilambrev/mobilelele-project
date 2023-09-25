3952db3c131dc483ddc34b29e352a309938c3f91
ilambrev <ilambrev@abv.bg>
2023-09-25 16:52:26 +0300

    Project initial Spring Boot configuration

---
+++ src/main/java/bg/softuni/mobileleleproject/MobileleleProjectApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.mobileleleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileleleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileleleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/mobileleleproject/MobileleleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.mobileleleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileleleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
