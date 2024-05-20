560940cb1b24d1af5905d7e4e08636b0b5d89499
ilambrev <ilambrev@abv.bg>
2024-05-20 17:37:54 +0300

    Spring Boot basic Security Configuration

---
+++ src/main/java/bg/softuni/mobileleleproject/config/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package bg.softuni.mobileleleproject.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                authorizeRequests -> authorizeRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/", "/users/login", "users/register").permitAll()
+                        .anyRequest().authenticated()
+        ).formLogin(
+                formLogin -> {
+                    formLogin
+                            .loginPage("/users/login")
+                            .usernameParameter("email")
+                            .passwordParameter("password")
+                            .defaultSuccessUrl("/")
+                            .failureForwardUrl("/users/login-error");
+                }
+        ).logout(
+                logout -> {
+                    logout
+                            .logoutUrl("/users/logout")
+                            .logoutSuccessUrl("/")
+                            .invalidateHttpSession(true);
+                }
+        );
+
+        return httpSecurity.build();
+    }
+
+}
